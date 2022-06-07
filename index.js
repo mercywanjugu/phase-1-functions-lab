f1c3e20e9d54dbac2305620cd01e84f23e1b7d52
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-07 14:29:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const hq = 42;
+const distanceFromHqInBlocks = (pickUpLocation) => {
+  return pickUpLocation < 42 ? hq - pickUpLocation : pickUpLocation - hq;
+};
+
+const distanceFromHqInFeet = (pickUpLocation) => {
+  return distanceFromHqInBlocks(pickUpLocation) * 264;
+};
+
+const distanceTravelledInFeet = (start, destination) => {
+  return destination < start
+    ? (start - destination) * 264
+    : (destination - start) * 264;
+};
+
+const calculatesFarePrice = (start, destination) => {
+  let charges;
+  if (distanceTravelledInFeet(start, destination) < 400) {
+    return 0;
+  } else if (
+    distanceTravelledInFeet(start, destination) >= 400 &&
+    distanceTravelledInFeet(start, destination) <= 2000
+  ) {
+    charges = (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+  } else if (
+    distanceTravelledInFeet(start, destination) > 2000 &&
+    distanceTravelledInFeet(start, destination) <= 2500
+  ) {
+    charges = 25;
+  } else {
+    return "cannot travel that far";
+  }
+  return charges;
+};
